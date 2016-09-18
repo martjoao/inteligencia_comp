dbcb5526d31a18074e263f5d09bb31134641dbea
João Martins <martins.j.neto@gmail.com>
2016-09-18 17:25:09 -0300

    Finish v0 parser

--- src/instance.cpp
+++ src/instance.cpp
@@ -1,17 +1,63 @@
 #include "instance.h"
 
 #include <fstream>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
+
 Instance::Instance() {}
+
 
 Instance* Instance::parseInstance(string filename) {
 
     Instance* i = new Instance();
-
     ifstream ifs(filename.c_str());
 
-    ifs >> i->nGateways;
+    //read quantities
+    ifs >> i->nGateways >> i->nClients >> i->nGroups >> i->gCapacity;
+   
+    //read requested bandwidths
+
+    for (int j = 0; j < i->nClients; j++) {
+        int cli;
+        ifs >> cli;
+        i->clientBandwidth.push_back(cli);    
+    }
+
+
+    int n;
+    string line;
+    getline(ifs, line);
+    getline(ifs, line);
+
+    //read ranges
+    for (int j = 0; j < i->nGateways; j++) {
+        i->gRanges.push_back(vector<int>());
+
+        getline(ifs, line);
+        istringstream iss(line);
+    
+        while (iss >> n) {
+            i->gRanges[j].push_back(n);
+        }
+    }    
+
+    getline(ifs, line);
+
+    //read groups
+    for (int j = 0; j < i->nGroups; j++) {
+        i->groups.push_back(vector<int>());
+             
+        getline(ifs, line);
+        istringstream iss(line);
+         
+        while (iss >> n) {
+            i->groups[j].push_back(n);
+        }   
+    }
+//*/
 
     ifs.close();
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,4 +8,29 @@ int main() {
     Instance *i = Instance::parseInstance("instances/instance1.txt");
 
     cout << i->nGateways << endl;
+    cout << i->nClients << endl;
+    cout << i->nGroups << endl;
+    cout << i->gCapacity << endl << endl;
+
+    for (int j =0; j < i->nClients; j++) {
+        cout << i->clientBandwidth[j] << " ";
+    }
+    cout << endl << endl;
+
+    for (int j = 0; j < i->nGateways; j++) {
+        for (int k = 0; k < i->gRanges[j].size(); k++) {
+            cout << i->gRanges[j][k] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+    
+    for (int j = 0; j < i->nGroups; j++) {
+        for (int k = 0; k < i->groups[j].size(); k++) {
+            cout << i->groups[j][k] << " ";
+        }
+        cout << endl;
+    }
+
 }
